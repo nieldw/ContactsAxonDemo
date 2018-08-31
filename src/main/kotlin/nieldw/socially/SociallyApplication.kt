6b61b6d5bf4b57d00cf00d261f5bd13c75bc2a04
Niel de Wet <nieldw@users.noreply.github.com>
2018-08-31 14:54:57 +0200

    Initial setup
    Includes:
    - Maven wrapper
    - Axon Spring Boot starter
    - Spring Boot starter JPA + H2
    - Spring Boot starter Thymeleaf
    - Spring Boot starter Web
    - Kotlin
    - JUnit 5

---
+++ src/main/kotlin/nieldw/socially/SociallyApplication.kt
@@ -0,0 +1,11 @@
+package nieldw.socially
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SociallyApplication
+
+fun main(args: Array<String>) {
+    runApplication<SociallyApplication>(*args)
+}
---
+++ src/test/kotlin/nieldw/socially/SociallyApplicationTests.kt
@@ -0,0 +1,16 @@
+package nieldw.socially
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest
+class SociallyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
